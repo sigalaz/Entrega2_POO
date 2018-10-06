91dbd94912bec67e2f455e93781fdfd02aa8f6a1
sigalaz <sigalaz@miuandes.cl>
2018-10-06 00:44:10 -0400

    Primera subida de proyecto

---
+++ Entrega2_POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2_POO
+{
+    class Program
+    {
+        public static void guardarPartida()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Jugador[] jugadores;
+            Bitmon[] bitmonBaneados;
+
+            
+        }
+    }
+}
