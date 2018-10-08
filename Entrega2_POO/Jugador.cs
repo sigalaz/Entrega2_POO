ead0a5e18d91c2d199ed2acce9fc163abc6bd94b
sigalaz <sigalaz@miuandes.cl>
2018-10-08 19:06:13 -0400

    Edicion clase jugador

--- Entrega2_POO/Jugador.cs
+++ Entrega2_POO/Jugador.cs
@@ -13,11 +13,31 @@ namespace Entrega2_POO
         int jugada;
         int tipoAtaque;
         bool EsTuTurno = false;
+        Bitmon bitmomActivo;
+        
 
+        
+        
+        
         public Jugador(string nombre)
         {
             this.nombre = nombre;
+            equipo[0].estaActivo = true;
         }
+
+        public void CambiarBitmom(Bitmon bitmomActivo)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (equipo[i].estaActivo == true) { equipo[i].estaActivo = false; }
+            }
+
+            bitmomActivo.estaActivo = true;
+
+
+        }
+
+
 
         public void agregarBitmon(Bitmon bitmon)
         {
@@ -26,35 +46,126 @@ namespace Entrega2_POO
 
         private void verEquipo()
         {
-            for (int i = 1; i < 4; i++)
+            for (int i = 0; i < 3; i++)
             {
                 equipo[i].VerInfoBitmom();
             }
         }
 
-        public void Estrategia()    // Es el metodo que permite al jugador elegir que hacer en el juego
-        {
-            bool error = true;
-            Console.WriteLine("Que movimiento deseas hacer {0}?:\n [1] Cambiar Bitmom activo \n [2] Atacar \n [3] Ver info Bitmoms ");
 
+        public void ElegirJugada(int NumOpciones)
+        {
+
+
+            bool error = true;
             while (error)
             {
                 try
                 {
-                    jugada = int32.Parse(Console.Read());
+                    jugada = int.Parse(Console.ReadLine());
                     error = false;
                 }
                 catch (Exception e)
                 {
+
+                    Console.WriteLine("Debe ingresar una opcion numerica!!");
                     Console.WriteLine(e.Message);
-                    Console.WriteLine("Hubo un error, ingrese nuevamente lo que decea hacer");
+
                 }
+                finally
+                {
+                    if ((jugada > NumOpciones)|| (jugada <= 0))
+                    {
+                        error = true;
+                        Console.WriteLine("Opcion no valida, ingrese nuevamente.");
+                    }
+                }
+
+            }
+        }
+
+
+
+
+
+
+        // Es el metodo que permite al jugador elegir que hacer en el juego
+        public void Estrategia()
+        {
+
+            Console.WriteLine("Que movimiento deseas hacer {0}?:\n [1] Cambiar Bitmom activo \n [2] Atacar \n [3] Ver info Bitmoms \n [4] Guardar Partida ");
+            ElegirJugada(4);
+
+
+            //[1] Cambiar Bitmom activo \n [2] Atacar \n [3] Ver info Bitmoms \n Descansar
+
+
+            if(jugada == 1)
+            {
+
+
+                Console.WriteLine("Indique el bitmom que desea utilizar: ");
+                verEquipo();    //muestra la info del equipo
+
+                ElegirJugada(3);
+
+                while (equipo[jugada].estaVivo() == false)
+                {
+
+                    Console.WriteLine("El Bitmom {0} ya esta muerto ", equipo[jugada]);
+                    Console.WriteLine("seleccione nuevamente");
+                    ElegirJugada(3);
+                }
+                Console.WriteLine("Has seleccionado el Bitmom {0}", equipo[jugada]);
+
+
+            } 
+                
+                
+            else if (jugada == 2)
+            {
+                equipo[jugada].Ataque();
 
 
 
             }
 
+            else if (jugada == 3)
+            {
+                verEquipo();
+
+            }
+
+            else if (jugada == 4){
+                
+
+                Console.WriteLine("Guardar partida");// FALTA POR HACER
+
+                //Guardar partida
+            }
+
+            
+
+
+
+            
 
         }
+
+        public bool KO()
+        {
+            if (equipo[0].estaVivo() && (equipo[1].estaVivo() && equipo[2].estaVivo()))
+            {
+                return true;
+            }
+
+
+            return false;
+
+        }
+            
+        }
+
+
     }
-}
+
---
+++ Entrega2_POO/Naturaleza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2_POO
+{
+    abstract class Naturaleza
+    {
+
+
+        public abstract List<int> Ataque();
+
+
+
+        
+
+
+        
+         
+         
+
+
+
+    }
+}
